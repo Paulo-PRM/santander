c48c1ab035f94943381956001525daee402ec244
Paulo-PRM <nerdrev@gmail.com>
2025-08-13 18:41:08 -0300

    Retirada de codigo comentado

--- src/main/java/br/com/stefanini/santander/santander/service/ValidarCepSolicitado.java
+++ src/main/java/br/com/stefanini/santander/santander/service/ValidarCepSolicitado.java
@@ -14,25 +14,4 @@ public class ValidarCepSolicitado {
                 .toList();
     }
 
-    /*public List<CepDTO> validarCepSolicitado(List<CepDTO> cepDTO, String cepDigitado) {
-        List<CepDTO> encontrados = new ArrayList<>();
-        cepDTO.stream()
-                .filter(cep -> cep.getCep().equals(cepDigitado))
-                .findFirst()
-                .ifPresentOrElse(cp -> {
-                    encontrados.add(CepDTO.builder()
-                                    .cep(cp.getCep())
-                                    .complemento(cp.getComplemento())
-                                    .logradouro(cp.getLogradouro())
-                                    .bairro(cp.getBairro())
-                                    .localidade(cp.getLocalidade())
-                                    .uf(cp.getUf())
-                                    .ddd(cp.getDdd())
-                            .build());
-                }, () -> {
-                    System.out.println("CEP não encontrado!!!!!!");
-                });
-        return encontrados;
-    }*/
-
 }
